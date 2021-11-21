97c354032ef98ebfa9d7fb652d053287e02c2f19
vera <verapoles1@mail.ru>
2021-11-21 15:26:18 +0200

    feat: implement task

---
+++ src/assets/js/index.js
@@ -0,0 +1,25 @@
+﻿import "@scss/styles.scss";
+
+window.addEventListener("load", function(event) {
+	document.getElementById("skew-btn").addEventListener("click", function() {
+		var inputValue = document.getElementById("inp").value;
+		if (inputValue === '') {
+			alert("Заполните поле!");
+			return;
+		}
+		fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify({
+    title: 'foo',
+    body: inputValue,
+    userId: 1,
+  }),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => response.json())
+  .then((json) => alert("Запрос выполнен успешно!"))
+	.catch((e) => alert("Произошла ошибка!"));
+	});
+});
